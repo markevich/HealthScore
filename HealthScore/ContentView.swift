fc7972e8fc4b147b0bb32960637d57278803440b
markevich <slava.markevich@gmail.com>
2021-06-11 17:16:11 +0300

    Initial Commit

---
+++ HealthScore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthScore
+//
+//  Created by Вячеслав Маркевич on 11.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HealthScore/HealthScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthScoreApp.swift
+//  HealthScore
+//
+//  Created by Вячеслав Маркевич on 11.06.21.
+//
+
+import SwiftUI
+
+@main
+struct HealthScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
